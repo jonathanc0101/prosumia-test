a1c8f238f9a07622dab44a65cb9d15e1f89d8a13
jonathanc0101 <jonathancavia0101@gmail.com>
2022-02-15 14:09:58 -0300

    pensado mejor el modelo del ej3

--- ej3.py
+++ ej3.py
@@ -32,3 +32,40 @@
 #  NO  N    NE NO  N    NE NO  N    NE NO  N    NE NO  N    NE
 #  O   Sim5  E O   Sim6  E O   Sim7  E O   Sim8  E O   Sim9  E ...
 #  SO  S    SE SO  S    SE SO  S    SE SO  S    SE SO  S    SE
+
+# un ejemplo de esto sería:
+# se lee la primera regla (A,S,B) del conjunto de reglas [(A,S,B),(B,N,A),(C,S,A)]
+
+#  NO  N    NE
+#  O   B    E
+#  SO  S    SE
+#  NO  N    NE
+#  O   A    E
+#  SO  S    SE
+
+#   dado que (A,S,B) implica (B,N,A) se lee recien la tercera regla:
+
+#  NO  N    NE
+#  O   B    E
+#  SO  S    SE
+#  NO  N    NE
+#  O   A    E
+#  SO  S    SE
+#  NO  N    NE
+#  O   C    E
+#  SO  S    SE
+
+# El conjunto de reglas termina siendo valido, ahora si le añadimos la regla (D,N,C) vemos que el conjunto no es valido, ya que el lugar al norte de C está ocupado por A
+
+# Si pensamos el problema en terminos de un plano XY, donde el simbolo inicial se coloca en el punto (0,0), el norte es (0,1), sur es (0,-1), este es (1,0), oeste es ...
+
+# constantes
+norte = [0, 1]
+sur = [0, -1]
+este = [1, 0]
+oeste = [0, 1]
+
+norOeste = [-1, 1]
+norEste = [1, 1]
+surOeste = [-1, -1]
+surEste = [1, -1]
