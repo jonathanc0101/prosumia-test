bb419a9a25dc6cf6576a1761f7311d5b2729a2f7
jonathanc0101 <jonathancavia0101@gmail.com>
2022-02-19 19:45:38 -0300

    se comprueba las reglas a 1 nivel de profundidad

--- ej3.py
+++ ej3.py
@@ -3,12 +3,38 @@
 # SO  S  SE
 
 
-opuestos = {"NO": "SE", "N": "S", "NE": "SE", "O": "E"}
+opuestos = {"NO": "SE", "N": "S", "NE": "SO", "O": "E"}
 todosLosOpuestos = {"NO": "SE", "SE": "NO", "N": "S",
-                    "S": "N", "NE": "SE", "SE": "NE", "O": "E", "E": "O"}
+                    "S": "N", "NE": "SO", "SO": "NE", "O": "E", "E": "O"}
 
 
 conjuntoTest = [("A", "S", "B"), ("B", "N", "A"), ("C", "S", "A")]
+# conjuntoTest = [("A", "S", "B"),
+#                 ("C", "S", "A"),
+#                 ("C", "NO", "A")]
+
+
+def opuesto(regla):
+    return((regla[2], todosLosOpuestos[regla[1]], regla[0]))
+
+
+def comprobarDiccionarioDeSimbolos(simbolos):
+    for simbolo in simbolos:
+        diccAux = {}
+        # comprobamos si hay mas de un mismo simbolo al que se va, con una direccion distinta
+        for tupla in simbolos[simbolo]:
+            direccion = tupla[0]
+            if direccion not in diccAux:
+                diccAux[direccion] = True
+            else:
+                return False
+
+            simboloTerminal = tupla[1]
+            if simboloTerminal not in diccAux:
+                diccAux[simboloTerminal] = True
+            else:
+                return False
+    return True
 
 
 def main(conjunto=conjuntoTest):
@@ -19,29 +45,42 @@ def main(conjunto=conjuntoTest):
 
     for regla in conjuntoTest:
 
-        if regla[1] in opuestos:  # cambiamos por el opuesto e invertimos el orden
-            conjuntoProcesado.append((regla[2], opuestos[regla[1]], regla[0]))
+        if regla[1] in opuestos:  # cambiamos por el opuesto
+            conjuntoProcesado.append(opuesto(regla))
         else:
-            conjuntoProcesado.append((regla[0], regla[1], regla[2]))
+            conjuntoProcesado.append(regla)
 
     # eliminamos los duplicados
     conjuntoProcesado = list(set(conjuntoProcesado))
 
     for regla in conjuntoProcesado:
         # colocamos las direcciones y simbolos a los que se refiere cada regla
-        if regla[0] not in simbolos:
-            simbolos[regla[0]] = [(regla[1], regla[2])]
+        simboloInicial = regla[0]
+        direccionOriginal = regla[1]
+        simboloTerminal = regla[2]
+        if simboloInicial not in simbolos:
+            simbolos[simboloInicial] = [(direccionOriginal, simboloTerminal)]
         else:
-            simbolos[regla[0]].append((regla[1], regla[2]))
+            simbolos[simboloInicial].append(
+                (direccionOriginal, simboloTerminal))
 
-        if regla[2] not in simbolos:
-            simbolos[regla[2]] = [(todosLosOpuestos[regla[1]], regla[0])]
+        if simboloTerminal not in simbolos:
+            simbolos[simboloTerminal] = [
+                (todosLosOpuestos[direccionOriginal], simboloInicial)]
         else:
-            simbolos[regla[0]].append((todosLosOpuestos[regla[1]], regla[0]))
+            simbolos[simboloTerminal].append(
+                (todosLosOpuestos[direccionOriginal], simboloInicial))
 
     print("Conjunto: ", conjunto)
     print("Conjunto procesado: ", conjuntoProcesado)
     print("Diccionario de simbolos: ", simbolos)
+
+    conjuntoValido = comprobarDiccionarioDeSimbolos(simbolos)
+
+    if conjuntoValido:
+        print("El conjunto de reglas es valido")
+    else:
+        print("El conjunto de reglas no es valido")
 
 
 if __name__ == "__main__":
